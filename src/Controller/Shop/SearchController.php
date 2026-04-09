84f152772386e3519546da0ee9d1cceea3df841b
teiling88 <teiling88@gmail.com>
2026-04-09 22:42:44 +0200

    NTR: finish search result page product section

--- src/Controller/Shop/SearchController.php
+++ src/Controller/Shop/SearchController.php
@@ -2,18 +2,51 @@
 
 namespace EilingIo\SyliusBatteryIncludedPlugin\Controller\Shop;
 
+use BatteryIncludedSdk\Shop\BrowseSearchStruct;
+use BatteryIncludedSdk\Shop\BrowseService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 
 class SearchController extends AbstractController
 {
-    public function search(): Response
+    public function __construct(
+        private readonly BrowseService $browseService,
+        private readonly ProductRepositoryInterface $productRepository
+    ) {
+    }
+
+    public function search(Request $request): Response
     {
-        // Hier kannst du beliebige Logik einbauen
-        return $this->render('@EilingIoSyliusBatteryIncludedPlugin/shop/search/search.html.twig', [
-            'message' => 'Hello from your custom Shop-Controller!'
-        ]);
+        $searchWord = $request->query->get('search');
+        $searchStruct = new BrowseSearchStruct();
+        $searchStruct->setQuery($searchWord);
+        $perPage = 9;
+        $currentPage = $request->query->getInt('page', 1);
+        $searchStruct->setPerPage($perPage);
+        $searchStruct->setPage($currentPage);
+
+        $result = $this->browseService->browse($searchStruct);
+        $maxHits = $result->getFound();
+        $maxPages = $result->getPages();
+
+        $orderNumbers = array_values(
+            array_filter(
+                array_map(
+                    static fn ($item) => $item['document']['_PRODUCT']['ordernumber'] ?? null,
+                    $result->getHits()
+                )
+            )
+        );
+
+        $products = [];
+        if (!empty($orderNumbers)) {
+            $products = $this->productRepository->findBy(['code' => $orderNumbers]);
+        }
+        return $this->render('@EilingIoSyliusBatteryIncludedPlugin/shop/search/search.html.twig',
+            compact('products', 'searchWord', 'currentPage', 'maxHits', 'maxPages')
+        );
     }
 }
 
