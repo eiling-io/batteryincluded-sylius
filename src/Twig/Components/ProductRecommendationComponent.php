86336be5391a678a0722ac65ce223c40100706fc
teiling88 <teiling88@gmail.com>
2026-04-13 13:59:13 +0200

    NTR: add recommendations on detail page

---
+++ src/Twig/Components/ProductRecommendationComponent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace EilingIo\SyliusBatteryIncludedPlugin\Twig\Components;
+
+use BatteryIncludedSdk\Recommendations\RecommendationsResponse;
+use BatteryIncludedSdk\Recommendations\RecommendationsService;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(
+    name: 'EilingIo:ProductRecommendation',
+    template: '@EilingIoSyliusBatteryIncludedPlugin/shop/product/recommendation.html.twig'
+)]
+final class ProductRecommendationComponent
+{
+    public ?ProductInterface $product = null;
+
+    public array $recommendations = [];
+
+    public function __construct(
+        private readonly RecommendationsService $recommendationsService,
+        private readonly ProductRepositoryInterface $productRepository
+    ) {
+    }
+
+    public function mount(ProductInterface|null $productDetail = null): void
+    {
+        if ($productDetail instanceof ProductInterface) {
+            $rawResult = $this->recommendationsService->recommendByIdentifier($productDetail->getId())->getRecommendations();
+            foreach ($rawResult as $key => $recommendations) {
+                if (count($recommendations) > 0) {
+                    $orderNumbers = array_values(
+                        array_filter(
+                            array_map(
+                                static fn($item) => $item['document']['_PRODUCT']['ordernumber'] ?? null,
+                                $recommendations
+                            )
+                        )
+                    );
+
+                    if (!empty($orderNumbers)) {
+                        $orderNumbers = array_slice($orderNumbers, 0, 4);
+                        $products = $this->productRepository->findBy(['code' => $orderNumbers]);
+                        $this->recommendations[$key] = $products;
+                    }
+                }
+            };
+        }
+    }
+}
